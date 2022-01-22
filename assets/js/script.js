2e54b921ce92149d3d7ade0bf061ba6cd9d50ebd
Jill Cunningham <jleighcunningham@gmail.com>
2022-01-21 19:00:35 -0500

    made changes to OneCall fetch and removed 5-day fetch since it excludes UVI

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,64 @@
 //API key: 6f8f0b3c82edaa7e1356d7dd6031e484//
 
 var apiKey = "6f8f0b3c82edaa7e1356d7dd6031e484";
-var chosenCity = [];
-var cityUserInputEl = document.getElementById("city-name");
-var searchButton = document.getElementById("button");
-var userInput = document.getElementById("input-box");
-var frameEl = document.querySelector("#frame-wrapper");
-var cityBucketEl = document.getElementById("city-bucket");
+var chosenCity = "hartford";
+// var cityUserInputEl = document.getElementById("city-name");
+// var searchButton = document.getElementById("button");
+// var userInput = document.getElementById("input-box");
+// var frameEl = document.querySelector("#frame-wrapper");
+// var cityBucketEl = document.getElementById("city-bucket");
 
-searchButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    console.log("click");
-    var userInput = document.getElementById("input-box").value;
+//     esearchButton.addEventListener("click", function (event) {
+// vent.preventDefault();
+//     console.log("click");
+//     var userInput = document.getElementById("input-box").value;
+// })
 
 //this is the fetch function for current weather data
-var currentWeatherUrl= "api.openweathermap.org/data/2.5/weather?q=" + 
+var currentWeatherUrl= "https://api.openweathermap.org/data/2.5/weather?q=" + 
 chosenCity + 
 "&appid=" 
-+ apiKey;
++ apiKey
++ "&units=imperial"
 
 //this returns current weather data based on user city input//
 fetch(currentWeatherUrl).then(function (response) {
-    console.log(response);
+ //   console.log(response);
     if (response.ok) {
         response.json().then(function (data) {
             console.log(data);
-            cityBucketEl.innerHTML = "";
-
-//add variable here to drill down API content that will display (create element, set attribute, add class list, append to child)//
+            console.log(data.main.humidity);
+            console.log (data.main.temp);
+            console.log(data.wind.speed);
+            getFiveDay(data.coord.lat, data.coord.lon);
+            //cityBucketEl.innerHTML = "";
+        })
+    }
+})
+function getFiveDay(lat,lon){
+var oneCallUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" +lat+ "&lon=" +lon+ "&exclude=minutely,hourly,alerts&units=imperial&appid=" +apiKey
+fetch (oneCallUrl).then(function(response) {
+    if (response.ok) {
+        response.json().then(function (data) {
+            console.log("this is the One Call API",data);
+        })
+    }
+})
+}
 
 //this is the fetch for 5 Day Forecast//
-var fiveDayForecastUrl = "api.openweathermap.org/data/2.5/forecast?q=" 
+var fiveDayForecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" 
 + chosenCity +
 "&appid=" 
 + apiKey;
+
 //this returns 5-day weather data based on user city input//
-fetch(fiveDayForecastUrl).then(function (response) {
-    console.log(response);
-    if (response.ok) {
-        response.json().then(function (data) {
-            console.log(data);
-            cityBucketEl.innerHTML = "";
+//fetch(fiveDayForecastUrl).then(function (response) {
+    //console.log(response);
+  //  if (response.ok) {
+        //response.json().then(function (data) {
+         //   console.log(data);
+           // cityBucketEl.innerHTML = "";
+       // })
+   // }
+//})
